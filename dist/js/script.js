457f39ce82aebeb3efe3cf64e88ad446867fc590
ant1po1e <symamaxim@gmail.com>
2024-09-27 18:03:30 +0700

    table responsive

--- dist/js/script.js
+++ dist/js/script.js
@@ -227,11 +227,11 @@ $(document).ready(function () {
             schedules[stageId].forEach(schedule => {
                 const row = `
                     <tr class="border-b bg-gray-800 border-gray-700">
-                        <td class="px-6 py-4">${schedule.date}</td>
-                        <td class="px-6 py-4">${schedule.time}</td>
-                        <td class="px-6 py-4">${schedule.referee}</td>
-                        <td class="px-6 py-4">${schedule.player}</td>
-                        <td class="px-6 py-4"><a class="underline" href="${schedule.mpLink}" target="_blank">click</a></td>
+                        <td class="px-3 py-2 md:px-6 md:py-4">${schedule.date}</td>
+                        <td class="px-3 py-2 md:px-6 md:py-4">${schedule.time}</td>
+                        <td class="px-3 py-2 md:px-6 md:py-4">${schedule.referee}</td>
+                        <td class="px-3 py-2 md:px-6 md:py-4">${schedule.player}</td>
+                        <td class="px-3 py-2 md:px-6 md:py-4"><a class="underline" href="${schedule.mpLink}" target="_blank">click</a></td>
                     </tr>
                 `;
                 $('#schedule-body').append(row);
