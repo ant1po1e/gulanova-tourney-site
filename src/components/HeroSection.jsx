5fc7bd173063218cfe35e6557fdc02ec30d33431
ant1po1e <symamaxim@gmail.com>
2026-05-10 16:16:05 +0700

    add socials button to hero section & hamburg animation

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,50 +1,73 @@
-import { Typewriter } from "react-simple-typewriter";
-
 export const HeroSection = () => {
     return (
         <section
             className="w-full px-4 md:px-24"
-            aria-label="Hero Section with introduction and social links"
-        >
+            aria-label="Hero Section with introduction and social links">
             {/* Title */}
             <div className="mx-auto text-center z-1 lg:hidden">
                 <img src="/bg.webp" className="rounded-lg" alt="" />
             </div>
 
             {/* Mobile social links */}
-            <div className="flex flex-wrap md:hidden justify-center gap-2 mt-4">
-                {/* GitHub dengan teks di kiri dalam kotak */}
+            <div className="md:hidden justify-center grid grid-cols-2 gap-2 mt-4">
                 <a
-                    href="https://github.com/ant1po1e"
-                    aria-label="GitHub"
+                    href="https://www.gulanova.top/twitch"
+                    aria-label="Twitch"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="px-3 h-10 rounded-lg bg-white flex flex-row items-center gap-2 shadow-md md:hover:bg-blue-400 focus:ring-2 focus:ring-blue-400 transition duration-300 group"
-                >
-                    <span className="text-sm font-medium text-black md:group-hover:text-white transition-colors duration-300">
-                        GitHub
-                    </span>
+                    className="px-3 h-10 rounded-lg fade-out-bg flex flex-row justify-center items-center gap-2 shadow-md focus:ring-2 focus:ring-blue-400 transition duration-300">
                     <i
-                        className="bi bi-github text-lg md:group-hover:text-2xl md:group-hover:text-white transition-all duration-300"
+                        className="bi bi-twitch text-lg text-white"
                         aria-hidden="true"
                     />
+                    <span className="text-sm font-medium text-white ">
+                        Twitch
+                    </span>
                 </a>
 
-                {/* YouTube dengan teks di kanan dalam kotak */}
                 <a
-                    href="https://www.youtube.com/@ant1po1e"
+                    href="https://www.gulanova.top/youtube"
                     aria-label="YouTube"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="px-3 h-10 rounded-lg bg-white flex flex-row-reverse items-center gap-2 shadow-md md:hover:bg-blue-400 focus:ring-2 focus:ring-blue-400 transition duration-300 group"
-                >
-                    <span className="text-sm font-medium text-black md:group-hover:text-white transition-colors duration-300">
-                        YouTube
-                    </span>
+                    className="px-3 h-10 rounded-lg fade-out-bg flex flex-row-reverse justify-center items-center gap-2 shadow-md  focus:ring-2 focus:ring-blue-400 transition duration-300">
                     <i
-                        className="bi bi-youtube text-lg md:group-hover:text-2xl md:group-hover:text-white transition-all duration-300"
+                        className="bi bi-youtube text-lg text-white"
                         aria-hidden="true"
                     />
+                    <span className="text-sm font-medium text-white ">
+                        YouTube
+                    </span>
+                </a>
+
+                <a
+                    href="https://www.gulanova.top/instagram"
+                    aria-label="Instagram"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-3 h-10 rounded-lg fade-out-bg flex flex-row justify-center items-center gap-2 shadow-md  focus:ring-2 focus:ring-blue-400 transition duration-300">
+                    <i
+                        className="bi bi-instagram text-lg text-white"
+                        aria-hidden="true"
+                    />
+                    <span className="text-sm font-medium text-white ">
+                        Instagram
+                    </span>
+                </a>
+
+                <a
+                    href="https://www.gulanova.top/discord"
+                    aria-label="Discord"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-3 h-10 rounded-lg fade-out-bg flex flex-row-reverse justify-center items-center gap-2 shadow-md  focus:ring-2 focus:ring-blue-400 transition duration-300">
+                    <i
+                        className="bi bi-discord text-lg text-white"
+                        aria-hidden="true"
+                    />
+                    <span className="text-sm font-medium text-white ">
+                        Discord
+                    </span>
                 </a>
             </div>
 
@@ -56,8 +79,7 @@ export const HeroSection = () => {
                     controlsList="nodownload"
                     autoPlay
                     muted
-                    loop
-                >
+                    loop>
                     <source src="/vid/vid.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
